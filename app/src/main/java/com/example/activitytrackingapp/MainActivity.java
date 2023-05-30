9703a6cd8a8ca27e58df875b8611c29ad1258b52
vissarionas <viss200012@gmail.com>
2023-05-30 17:48:04 +0300

    First Commite

---
+++ app/src/main/java/com/example/activitytrackingapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.activitytrackingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void setBackgroundColor() {
+
+    }
+}
