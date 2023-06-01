337f14bdb9b5f3d91b151719836df7b136e5baea
vissarionas <viss200012@gmail.com>
2023-06-01 17:54:04 +0300

    The app can open file picker!

--- app/src/main/java/com/example/activitytrackingapp/MainActivity.java
+++ app/src/main/java/com/example/activitytrackingapp/MainActivity.java
@@ -1,8 +1,21 @@
 package com.example.activitytrackingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.GnssAntennaInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,9 +23,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button uploadFileBTN = findViewById(R.id.uploadFileBTN);
+
+        uploadFileBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if(checkPermissions()){
+                    //permission allowed
+                    openFile(myUri);
+                }else{
+                    //permission not allowed
+                    requestPermissions();
+                }
+            }
+        });
     }
 
-    public void setBackgroundColor() {
+    Uri myUri = Uri.parse("/STORAGE/SDCARD");
 
+    private void openFile(Uri pickerInitialUri) {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("application/pdf");
+
+        // Optionally, specify a URI for the file that should appear in the
+        // system file picker when it loads.
+        intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri);
+
+        startActivityForResult(intent, 2);
     }
+
+    private boolean checkPermissions(){
+        int result = ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.READ_EXTERNAL_STORAGE);
+        if(result == PackageManager.PERMISSION_GRANTED){
+            return true;
+        }else
+            return false;
+    }
+
+    private void requestPermissions() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+            Toast.makeText(MainActivity.this,"Απετείτε αδεία προσβασής στα αρχεία της συσκευής",Toast.LENGTH_SHORT).show();
+        }else
+        ActivityCompat.requestPermissions(MainActivity.this,new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},111);
+    }
+
 }
