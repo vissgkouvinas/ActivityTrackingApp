a28f02b7ce88ffda0d9a67bb182de1342184090a
vissarionas <viss200012@gmail.com>
2023-06-03 17:47:46 +0300

    Successfully reading the file path.

--- app/src/main/java/com/example/activitytrackingapp/MainActivity.java
+++ app/src/main/java/com/example/activitytrackingapp/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.activitytrackingapp;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -45,13 +48,24 @@ public class MainActivity extends AppCompatActivity {
     private void openFile(Uri pickerInitialUri) {
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
-        intent.setType("application/pdf");
+        intent.setType("application/octet-stream");
 
         // Optionally, specify a URI for the file that should appear in the
         // system file picker when it loads.
         intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri);
 
-        startActivityForResult(intent, 2);
+        startActivityForResult(intent,2);
+    }
+
+    String pathOfChosenFile;
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if(requestCode == 2) {
+            Uri currFileURI = data.getData();
+            pathOfChosenFile = currFileURI.getPath();
+            Toast.makeText(MainActivity.this,pathOfChosenFile,Toast.LENGTH_SHORT).show();
+        }
     }
 
     private boolean checkPermissions(){
@@ -64,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void requestPermissions() {
         if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,Manifest.permission.READ_EXTERNAL_STORAGE)){
-            Toast.makeText(MainActivity.this,"Απετείτε αδεία προσβασής στα αρχεία της συσκευής",Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this,"Απαιτείται άδεια πρόσβασης στα αρχεία της συσκευής!",Toast.LENGTH_SHORT).show();
         }else
         ActivityCompat.requestPermissions(MainActivity.this,new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},111);
     }
